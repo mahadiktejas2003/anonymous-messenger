e84a116d59f474b97b37a43cd0da2632caed07d5
mahadiktejas2003 <mahadiktejas165@gmail.com>
2025-01-17 23:14:19 +0530

    eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,22 @@
+// eslint.config.js
+
 export default [
   {
-      rules: {
-          semi: "error",
-          "prefer-const": "error"
-      }
-  }
+    files: ['**/*.ts', '**/*.tsx'], // Specify file types
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+    },
+  },
 ];
