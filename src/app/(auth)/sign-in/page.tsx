9aff49af94a1ae1d048466b806a3ce42a9b2ba30
mahadiktejas2003 <mahadiktejas165@gmail.com>
2025-01-18 00:19:39 +0530

    sign in correction

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,212 +1,116 @@
 'use client'
-import MessageCard from '@/components/MessageCard'
-import { Button } from '@/components/ui/button'
-import { Switch } from '@/components/ui/switch'
-import { toast, useToast } from '@/hooks/use-toast'
-import { Message } from '@/model/User'
-import { AcceptMessageSchema } from '@/schemas/acceptMessageSchema'
-import { ApiResponse } from '@/types/ApiResponse'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Separator } from '@radix-ui/react-separator'
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from 'next/link'
+import {useDebounceCallback} from 'usehooks-ts'
+import { useToast } from "@/hooks/use-toast"
+import { useState } from "react"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import { useRouter } from "next/navigation"
 import axios, { AxiosError } from 'axios'
-import { Loader2, RefreshCcw } from 'lucide-react'
-import { User } from 'next-auth'
-import { useSession } from 'next-auth/react'
-import React, { useCallback, useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { ApiResponse } from "@/types/ApiResponse"
+import { useEffect } from "react"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+import { signInSchema } from "@/schemas/signInSchema"
+import { signIn } from "next-auth/react"
 
-// Ensure the component name starts with an uppercase letter
-export default function SignInForm() { // Corrected the component name to 'Page'
-  //states
-  const [messages, setMessages] = useState<Message[]>([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [isSwitchLoading, setIsSwitchLoading] = useState(false)
+const Page = () => {
 
-  const { toast } = useToast()
-  function handleDeleteMessage(messageId: string) {
-    console.log("delete start from UI")
-    setMessages(messages.filter((message) => message._id !== messageId))
-  }
+    const {toast} = useToast()
+    const router= useRouter()
+    //Zod implementation
 
-  const { data: session } = useSession()
-
-  //form
-  const { register, watch, setValue } = useForm({
-    resolver: zodResolver(AcceptMessageSchema)
-  })
-
-  const acceptMessages = watch('acceptMessages')
-
-  //API calls
-
-  //accceptMessage status fetching
-  const fetchAcceptMessage = useCallback(async () => {
-    setIsSwitchLoading(true);
-    try {
-      const response = await axios.get<ApiResponse>('/api/accept-messages')
-
-      console.log('Success--ACCEPT MESSAGE RESPONSE is:', response);
-
-      setValue('acceptMessages', response.data.isAcceptingMessages) // setValue: Dynamically updates form values
-    }
-    catch (error) {
-      console.log('THE DASHBOARD-- Error ACCEPT MESSAGE RESPONSE');
-
-      const axiosError = error as AxiosError<ApiResponse>;
-      toast({
-        title: 'Error',
-        description: axiosError.response?.data.message ||
-          'Failed to fetch Message Settings',
-        variant: 'destructive'
-      })
-    }
-    finally {
-      setIsSwitchLoading(false)
-    }
-  }, [setValue])
-
-  //Purpose: Fetches all messages via an API call and updates the state.
-  const fetchMessages = useCallback(async (refresh: boolean = false) => {
-    setIsLoading(true)
-    setIsSwitchLoading(false)
-    try {
-      const response = await axios.get<ApiResponse>('api/get-messages')
-      setMessages(response.data.messages || [])
-      if (refresh) {
-        toast({
-          title: 'Refreshed Messages',
-          description: 'Showing latest messages'
-        })
-      }
-    } catch (error) {
-      const axiosError = error as AxiosError<ApiResponse>;
-      toast({
-        title: 'Error',
-        description: axiosError.response?.data.message || 'Failed to fetch message settings',
-        variant: 'destructive'
-      })
-
-    }
-    finally {
-      setIsLoading(false)
-      setIsSwitchLoading(false) //TODO: try changing this
-    }
-
-  }, [setIsLoading, setMessages])
-
-  // Fetch initial state from the server
-  useEffect(() => {
-    if (!session || !session.user) return
-    fetchMessages() //fetching the messages
-    fetchAcceptMessage() //state if isAcceptingMessages
-
-  }, [session, setValue, fetchAcceptMessage, fetchMessages])
-
-  // Handle switch change
-
-  const handleSwitchChange = async () => {
-    try {
-      const response = await axios.post<ApiResponse>('api/accept-messages', {
-        acceptMessages: !acceptMessages
-      });
-      console.log('HANDLE SWITCH- accept message', response);
-
-      console.log("THE RESPONSE ACCEPT MESSAGE UPDATED- ", !acceptMessages)
-      setValue('acceptMessages', !acceptMessages);
-      toast({
-        title: response.data.message,
-        variant: 'default',
-      });
-
-    }
-    catch (error) {
-      const axiosError = error as AxiosError<ApiResponse>;
-      toast({
-        title: 'Error',
-        description:
-          axiosError.response?.data.message || 'Failed to update the message settings',
-        variant: 'destructive'
-      });
-    }
-  };
-
-  if (!session || !session.user) {
-    return <div></div>;
-  }
-
-  const username = session?.user as User; //getting username
-  console.log('username', username)
-  //Setting up the URL for anonymous users to send messages
-
-  const baseUrl = `${window.location.protocol}//${window.location.host}`
-  const profileUrl = `${baseUrl}/u/${username.username}`;
-
-  function copyToClipboard() {
-    navigator.clipboard.writeText(profileUrl);
-    toast({
-      title: 'URL Copied',
-      description: 'Profile URL has been copied to clipboard.',
+    const form= useForm<z.infer<typeof signInSchema> >({
+        resolver: zodResolver(signInSchema),
+        defaultValues:{
+            identifier:'',
+            password:''
+        }
     })
-  }
+    //sign-in implementsthe NextAuth - > so write like this:
+      const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+         
+        const result= await signIn('credentials',{
+          redirect:false,
+          identifier:data.identifier,
+          password: data.password          
+        }) 
+        //if result has optionally error, then-
+        if(result?.error){
+          toast({
+            title:"Login failed",
+            description:"Incorrect username or password",
+            variant:"destructive"
+          })
+        }
+        if(result?.url){
+          router.replace('/dashboard')
+        }
+        
+        
+        
+      }
 
 
-  return (
-    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
-      <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
 
-      <div className="mb-4">
-        <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>{' '}
-        <div className="flex items-center">
-          <input
-            type="text"
-            value={profileUrl}
-            disabled
-            className="input input-bordered w-full p-2 mr-2"
-          />
-          <Button onClick={copyToClipboard}>Copy</Button>
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+          <div className="text-center">
+            <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+              Join Mystery Message
+            </h1>
+            <p className="mb-4">Sign in to start your anonymous adventure</p>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+  
+              <FormField
+                control={form.control}
+                name="identifier"
+                render={({ field }) => (
+                    <FormItem>
+                    <FormLabel>Email/Username</FormLabel>
+                        <Input placeholder="email/username" {...field} //any one of email or username is credential
+                          /> 
+                    <FormMessage />
+                    </FormItem>
+                )}
+            />
+
+            <FormField  
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" placeholder="password" {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">
+              Signin
+            </Button>
+            </form>
+          </Form>
+          <div className="text-center mt-4">
+            <p>Not a member?{' '}
+                <Link href='sign-up' className='text-blue-600 hover:text-blue-800'>Sign up</Link>
+            </p>
+
+          </div>
         </div>
       </div>
-
-      <div className="mb-4">
-        <Switch
-          {...register('acceptMessages')}
-          checked={acceptMessages}
-          onCheckedChange={handleSwitchChange}
-          disabled={isSwitchLoading}
-        />
-        <span className="ml-2">
-          Accept Messages: {acceptMessages ? 'On' : 'Off'}
-        </span>
-      </div>
-      <Separator />
-
-      <Button
-        className="mt-4"
-        variant="outline"
-        onClick={(e) => {
-          e.preventDefault();
-          fetchMessages(true);
-        }}
-      >
-        {isLoading ? (
-          <Loader2 className="h-4 w-4 animate-spin" />
-        ) : (
-          <RefreshCcw className="h-4 w-4" />
-        )}
-      </Button>
-      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
-        {messages.length > 0 ? (
-          messages.map((message) => (
-            <MessageCard
-              key={message._id as string}
-              message={message}
-              onMessageDelete={handleDeleteMessage}
-            />
-          ))
-        ) : (
-          <p>No messages to display.</p>
-        )}
-      </div>
-    </div>
+      
+          
   )
 }
+
+export default Page;
+
